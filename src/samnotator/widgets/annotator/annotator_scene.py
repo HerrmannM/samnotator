8e47d0872e51fd9b15f31ca8f35ad95161dfbeb7
HerrmannM <2685294+HerrmannM@users.noreply.github.com>
2025-12-07 15:44:33 +1100

    add layer per instance in scene

--- src/samnotator/widgets/annotator/annotator_scene.py
+++ src/samnotator/widgets/annotator/annotator_scene.py
@@ -2,6 +2,8 @@
 # --- --- --- Imports --- --- ---
 # STD
 from typing import cast
+from enum import IntEnum
+from dataclasses import dataclass
 # 3RD
 from PySide6.QtCore import Qt, QObject, Signal, Slot, QRectF, QPointF
 from PySide6.QtGui import QPixmap, QPainter
@@ -15,6 +17,36 @@ from samnotator.controllers.instance_controller import InstanceController
 from samnotator.datamodel import FrameID, InstanceID
 from samnotator.datamodel import PointAnnotation, PointKind, PointID, PointXY, Point
 from samnotator.utils._CUD import CUD
+
+
+class ZValues(IntEnum):
+    BACKGROUND = 0
+    MASK = 1
+    BBOX = 2
+    POINTS = 3
+    SELECTED_BBOX = 4
+    SELECTED_POINTS = 5
+#
+
+
+
+class LayerItem(QGraphicsItem):
+    def __init__(self, isntance_id:InstanceID, parent=None):
+        super().__init__(parent)
+        self.instance_id = isntance_id
+        # No visual contents: this item only serves as a container
+        self.setFlag(QGraphicsItem.GraphicsItemFlag.ItemHasNoContents, True)
+
+    def boundingRect(self) -> QRectF:
+        # Not used (no painting), but must be implemented
+        return QRectF()
+
+    def paint(self, painter, option, widget=None):
+        # No painting
+        pass
+    #
+#
+
 
 
 class QXItemPoint(QGraphicsPixmapItem):
@@ -37,7 +69,7 @@ class QXItemPoint(QGraphicsPixmapItem):
     # End of def __init__
 
 
-    def update(self, pixmap:QPixmap|None, point_xy:PointXY|None, visible=True) -> None:
+    def update(self, pixmap:QPixmap|None, point_xy:PointXY|None) -> None:
         if pixmap is not None:
             self.setPixmap(pixmap)
 
@@ -46,8 +78,6 @@ class QXItemPoint(QGraphicsPixmapItem):
             x,y = point_xy
             self.setPos(x+0.5, y+0.5)
             self.setOffset(-center.x(), -center.y())
-
-        self.setVisible(visible)
     # End of def update
 
 
@@ -105,6 +135,36 @@ class QXItemPoint(QGraphicsPixmapItem):
 # End of class QXItemPoint
 
 
+@dataclass(slots=True)
+class Layer:
+    instance_id:InstanceID
+    # Layers:
+    layer_points:LayerItem
+    layer_bbox:LayerItem
+    layer_mask:LayerItem
+    #
+    point_items:dict[PointID, QXItemPoint]
+    markers:list[QPixmap]
+    mask:QGraphicsPixmapItem|None
+    #
+
+    # --- --- --- Helpers --- --- ---
+
+    @classmethod
+    def default(cls, instance_id:InstanceID) -> Layer:
+        lp = LayerItem(instance_id)
+        lp.setZValue(ZValues.POINTS)
+        #
+        lb = LayerItem(instance_id)
+        lb.setZValue(ZValues.BBOX)
+        #
+        lm = LayerItem(instance_id)
+        lm.setZValue(ZValues.MASK)
+        #
+        return cls(instance_id = instance_id, layer_points = lp, layer_bbox = lb, layer_mask = lm, point_items = {}, markers = [], mask = None)
+    # End of classmethod def default
+#
+
 
 class AnnotatorScene(QGraphicsScene):
 
@@ -125,9 +185,7 @@ class AnnotatorScene(QGraphicsScene):
         self.frame_id:FrameID = frame_id
         self.qpixmap:QPixmap = qpixmap
         #
-        self.point_items:dict[PointID, QXItemPoint] = {}
-        self.instance_points:dict[InstanceID, set[PointID]] = {}
-        self.instance_mask:dict[InstanceID, QGraphicsPixmapItem] = {}
+        self.instance_layers:dict[InstanceID, Layer] = {}
 
         # Setup
         self._init_ui()
@@ -139,6 +197,7 @@ class AnnotatorScene(QGraphicsScene):
         # Connect: controller -> scene
         self.annotations_controller.point_list_changed.connect(self.on_point_list_changed)
         self.instance_controller.instance_changed.connect(self.on_instance_changed)
+        self.instance_controller.current_instance_changed.connect(self.on_current_instance_changed)
     # End of def _init_ui
 
     
@@ -166,45 +225,61 @@ class AnnotatorScene(QGraphicsScene):
     def on_instance_changed(self, instance_id:InstanceID, cud:CUD) -> None:
         match cud:
             case CUD.CREATE:
-                pass
+                self.instance_layers[instance_id] = Layer.default(instance_id=instance_id)
+                self.addItem(self.instance_layers[instance_id].layer_mask)
+                self.addItem(self.instance_layers[instance_id].layer_bbox)
+                self.addItem(self.instance_layers[instance_id].layer_points)
 
             case CUD.UPDATE:
                 instance = self.instance_controller.get(instance_id)
-                # Marks
-                mark_visible = instance.show_markers
-                marks:list[QPixmap] = self.instance_controller.get(instance_id).point_marks
-                for pid in self.instance_points.get(instance_id, set()):
-                    item = self.point_items[pid]
-                    item.update(marks[item.point_kind], None, visible=mark_visible)
-                # Mask - not the best but ok for now (re-render entire mask on any change)
-                if instance.show_mask:
-                    if (m := self.instance_controller.get_mask_for(instance_id, self.frame_id, instance.show_plain_mask)) is not None:
-                        if instance_id in self.instance_mask:
-                            item = self.instance_mask[instance_id]
-                            item.setPixmap(m)
-                            item.setVisible(True)
-                        else:
-                            mask_item = QGraphicsPixmapItem(m)
-                            mask_item.setZValue(-1)  # behind points
-                            self.addItem(mask_item)
-                            self.instance_mask[instance_id] = mask_item
-                else:
-                    if instance_id in self.instance_mask:
-                        item = self.instance_mask[instance_id]
-                        item.setVisible(False)
-            #
+                instance_layer = self.instance_layers[instance_id]
 
+                # --- Marks
+                # - Visibility
+                instance_layer.layer_points.setVisible(instance.show_markers)
+                # - Update marks
+                mark_pixmaps_request:list[QPixmap] = instance.point_marks
+                if mark_pixmaps_request != instance_layer.markers:
+                    for item in instance_layer.point_items.values():
+                        item.update(mark_pixmaps_request[item.point_kind], None)
+
+                # --- Mask
+                # - Visbility
+                instance_layer.layer_mask.setVisible(instance.show_mask)
+                # - Update mask: for now, full re update
+                mask = self.instance_controller.get_mask_for(instance_id, self.frame_id, instance.show_plain_mask)
+                if mask is not None:
+                    if instance_layer.mask is None: # New mask
+                        mask_item = QGraphicsPixmapItem(mask, parent = instance_layer.layer_mask)
+                        mask_item.setZValue(ZValues.MASK)  # behind points
+                        instance_layer.mask = mask_item
+                    else: # Update existing mask
+                        instance_layer.mask.setPixmap(mask)
+                    #
+                #
+                    
             case CUD.DELETE:
-                # Marker
-                for pid in list(self.instance_points.get(instance_id, set())): # make a copy as a list since we modify the set
-                    self.delete_point_annotation(pid, instance_id)
-                # Mask
-                if instance_id in self.instance_mask:
-                    item = self.instance_mask.pop(instance_id)
-                    self.removeItem(item)
+                instance_layer = self.instance_layers.pop(instance_id)
+                self.removeItem(instance_layer.layer_points)
+                self.removeItem(instance_layer.layer_bbox)
+                self.removeItem(instance_layer.layer_mask)
             #
         #
     #
+
+
+    @Slot(object)
+    def on_current_instance_changed(self, instance_id:InstanceID|None) -> None:
+        """Set the instance with `instance_id` as selected (or None for no selection), updating Z values accordingly."""
+        for iid, layer in self.instance_layers.items():
+            if iid == instance_id:
+                layer.layer_points.setZValue(ZValues.SELECTED_POINTS)
+                layer.layer_bbox.setZValue(ZValues.SELECTED_BBOX)
+            else:
+                layer.layer_points.setZValue(ZValues.POINTS)
+                layer.layer_bbox.setZValue(ZValues.BBOX)
+            #
+    # End of def select_layer
 
 
     # --- --- --- Display --- --- ---
@@ -220,34 +295,32 @@ class AnnotatorScene(QGraphicsScene):
     def add_point_annotation(self, pa:PointAnnotation) -> None:
         pid, iid = pa.point_id, pa.instance_id
         kind = pa.point.kind
-        assert pid not in self.point_items, f"Point ID {pid} already exists in the scene."
         # Set visibility according to instance settings
         instance = self.instance_controller.get(iid)
         if not instance.show_markers:
             self.instance_controller.update_instance(iid, show_markers=True)
-        # Create item
+        # Create item in its layer
+        layer = self.instance_layers[iid]
+        assert pid not in layer.point_items, f"Point ID {pid} already exists in the scene."
         mark:QPixmap = self.instance_controller.get(iid).point_marks[kind]
-        point_item = QXItemPoint(pid, kind, iid, mark, pa.point.position)
-        self.point_items[pid] = point_item
-        self.instance_points.setdefault(iid, set()).add(pid)
-        self.addItem(point_item)
-
+        point_item = QXItemPoint(pid, kind, iid, mark, pa.point.position, parent=layer.layer_points)
+        layer.point_items[pid] = point_item
     # End of def add_point_annotation
 
     
     def update_point_annotation(self, pa:PointAnnotation) -> None:
         mark:QPixmap = self.instance_controller.get(pa.instance_id).point_marks[pa.point.kind]
-        self.point_items[pa.point_id].update(mark, pa.point.position)
+        self.instance_layers[pa.instance_id].point_items[pa.point_id].update(mark, pa.point.position)
     # End of def update_point_annotation
 
 
     def delete_point_annotation(self, pid:PointID, iid:InstanceID) -> None:
-        assert pid in self.point_items, f"Point ID {pid} does not exist in the scene."
-        point_item = self.point_items.pop(pid)
-        self.instance_points[iid].remove(pid)
+        layer = self.instance_layers[iid]
+        assert pid in layer.point_items, f"Point ID {pid} does not exist in the scene."
+        point_item = layer.point_items.pop(pid)
         self.removeItem(point_item)
     # End of def delete_point_annotation
-    
+
 
     # --- --- --- Overrides --- --- ---
 
